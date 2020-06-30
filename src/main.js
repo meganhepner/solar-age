d44b27bda159adcc444b80b3a223e8acb13c05e5
Megan Hepner <megan.hepner@gmail.com>
2020-06-29 21:30:11 -0700

    finish UI code for age on other planets, save UI for LifeExpectancy for another time

--- src/main.js
+++ src/main.js
@@ -2,26 +2,27 @@ import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles.css';
-import { Age, checkInput, calculateMercury, calculateVenus, calculateMars, calculateJupiter } from './solar-age.js';
+import { Age } from './solar-age.js';
 
 $(document).ready(function () {
   $("form#ageLifeExpectancy").submit(function(event) {
     event.preventDefault();
-    // let earthLife = $("input#life").val();
     let earthAge = $("input#age").val();
+    let earthLife = $("input#life").val();
     let age = new Age(earthAge);
-    if (checkInput(earthAge) === false) {
+    if (age.checkInput === false) {
       $("div#ageAnswer").text("Please enter your age in whole numbers!");
     } else {
       age.calculateMercury();
-      calculateVenus(earthAge);
-      calculateMars(earthAge);
-      calculateJupiter(earthAge);
-    }
+      age.calculateVenus();
+      age.calculateMars();
+      age.calculateJupiter();
       $("div#ageAnswer").append(`<li> On Earth you would be ${age.earthAge} </li>`);
       $("div#ageAnswer").append(`<li> On Mercury you would be ${age.mercuryAge} </li>`);
       $("div#ageAnswer").append(`<li> On Venus you would be ${age.venusAge} </li>`);
       $("div#ageAnswer").append(`<li> On Mars you would be ${age.marsAge} </li>`);
       $("div#ageAnswer").append(`<li> On Jupiter you would be ${age.jupiterAge} </li>`); 
+    }
+    
   });
 });
--- src/solar-age.js
+++ src/solar-age.js
@@ -34,27 +34,16 @@ export class Age {
       this.MercuryExpectancy = Math.round(this.LifeExpectancy * 4.17);
       this.VenusExpectancy = Math.round(this.LifeExpectancy * 1.61);
       this.MarsExpectancy = Math.round(this.LifeExpectancy * .53);
-      this.JupiterExpectancy = Math.round(this.LifeExpectancy *.08)
+      this.JupiterExpectancy = Math.round(this.LifeExpectancy *.08);
     } else if (life - this.earthAge < 0) {
       this.LifeOverage = Math.abs(life - this.earthAge);
       this.MercuryOverage = Math.round(this.LifeOverage * 4.17);
       this.VenusOverage = Math.round(this.LifeOverage * 1.61);
       this.MarsOverage = Math.round(this.LifeOverage * .53);
-      this.JupiterOverage = Math.round(this.LifeOverage *.08)
+      this.JupiterOverage = Math.round(this.LifeOverage *.08);
     } else if (life === this.earthAge) {
       this.LifeEven = 0;
     }
   }
-
-  calculateMercuryLifeExpectancy(earthLifeExpectancy) {
-    if (this.LifeExpectancy) {
-      this.mercuryExpectancy = (this.LifeExpectancy * 4.17);
-    } else if (this.LifeOverage) {
-      this.mercuryOverage = [2, Math.abs(life - this.earthAge)];
-    } else if (life === this.earthAge) {
-      this.LifeExpectancy = [3, 0];
-    }
-  }
-
 }
   
